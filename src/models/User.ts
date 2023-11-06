81b01d558e7edbd290b9a82e1cd81847f1374043
feliperdamaceno <feliperdamaceno@sent.com>
2023-11-06 23:32:42 +0000

    feat: Create application boilerplate.

---
+++ src/models/User.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose'
+
+const userSchema = new Schema({
+  title: String,
+  author: String,
+  body: String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+export default User
